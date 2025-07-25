41e68930fa53c8f4dc0ed199e2f956c99bfca57d
Dizaraj Dey <dizaraj@gmail.com>
2025-07-25 23:47:52 +0600

    UI Updates

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ $(document).ready(function () {
     ],
   });
 });
+
+// Back to Top Button Logic
+var mybutton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
